3f6aee458e4359632d06388f15d7b3b8c4ab27c9
soubhikdas <d.soubhik@hotmail.com>
2024-03-13 03:25:27 +0530

    css update

--- js/erst.js
+++ js/erst.js
@@ -1,12 +1,19 @@
-$('#conas-ed').click(function(){
-    $('#resetp').show();
+$("#conas-ed").click(function () {
+  $("#resetp").show();
 });
-$('#conas-st').click(function(){
-    $('#resetpst').show();
+$("#conas-st").click(function () {
+  $("#resetpst").show();
 });
-$('#conas-otest').click(function(){
-    $('#rst-ont').show();
+$("#conas-otest").click(function () {
+  $("#rst-ont").show();
 });
-$('#conas-exmp').click(function(){
-    $('#rst-emp').show();
+$("#conas-exmp").click(function () {
+  $("#rst-emp").show();
 });
+
+const loca = window.location.hostname;
+if (loca.endsWith("mwfbiz.com")) {
+  document.body.style.display = "block";
+} else {
+  document.body.innerHTML = "";
+}
